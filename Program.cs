2a6abb68d3fa9aecc65bb69afb6f45006ef4e05f
Kyle Gabriel <kgabriel@email.sc.edu>
2025-09-02 18:24:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FileProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string directory = $"{args[0]}";
+            string[] files= Directory.GetFiles(directory);
+            //Loop through your files
+            foreach(File file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+        }
+    }
+}
